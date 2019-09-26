494971c03bb891fae2e5f024055d564390dc9205
linus.lindstrom <linus.lindstrom2@elev.ga.ntig.se>
2019-09-26 13:22:40 +0200

    R2 Läxa

---
+++ kattis/R2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class R2 {
+
+	private static Scanner input;
+
+	public static void main(String[] args) {
+
+		input = new Scanner(System.in);
+
+		int r1 = input.nextInt();
+
+		int s = input.nextInt();
+
+		System.out.println( s * 2 - r1);
+
+		
+	}
+
+}
